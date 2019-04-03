a8fa67be366a46fcbc0182fafe653cb84d0475ac
Andreas Streichardt <andreas@mop.koeln>
2019-04-03 07:47:40 +0200

    ✨ Current work

--- _plugins/NavigationBlock.rb
+++ _plugins/NavigationBlock.rb
@@ -3,52 +3,64 @@ require 'json'
 class NavigationBlock < Liquid::Block
     def initialize(tag_name, input, tokens)
         super
+        @prefix = input.strip
     end
 
-    def renderTree(root, stack)
+    def renderTree(context, root, stack)
         if root.length == 0
             return ""
         end
         indent = "    " * stack.length
 
+        found = false
+        output = ""
+        root.each.with_index { |element, index |
+            localIndent = indent + "  "
+            if element['subtitle']
+                output += localIndent + "<li class=\"header\">" + element['subtitle'] + "</li>\n"
+            elsif element['divider']
+                output += localIndent + "<li><hr/></li>\n"
+            else
+                children = ""
+                classNames = "chapter"
+                if element["href"]
+                    children += localIndent + "<a href=\"" + element['href'] + "\">" + element["text"] + "</a>\n"
+                    if context.environments.first["page"]["url"] == @prefix + "/" + element["href"]
+                        found = true
+                        classNames += " selected active"
+                    end
+                end
+                if element["children"]
+                    localChildren, localFound = renderTree(context, element['children'], stack + [index])
+                    children += localChildren
+                    if localFound
+                        found = true
+                        classNames += " selected"
+                    end
+                end
+                
+                output += localIndent + "<li class=\"" + classNames + "\">\n"
+                output += children
+                output += localIndent + "</li>\n"
+            end
+        }
         classNames = ""
         if stack.length == 0
             classNames += "summary"
         else
             classNames += "articles"
         end
-        output = indent + "<ul class=\"" + classNames + "\">\n"
-        level = "1"
-        stack.each { |prev_level|
-            level += "." + prev_level.to_s
-        }
-        root.each.with_index { |element, index |
-            localIndent = indent + "  "
-            output += localIndent + "<li class=\"chapter\" data-level=\"" + level + "." + (index + 1).to_s + "\">\n"
-            if element['href']
-                output += localIndent + "<a href=\"" + element['href'] + "\">" + element["text"] + "</a>\n"
-            end
-            if element['subtitle']
-                output += localIndent + "<li class=\"header\">" + element['subtitle'] + "</li>\n"
-            end
-            if element['children']
-                output += renderTree(element['children'], stack + [index])
-            end
-            if element['divider']
-                output += localIndent + "<hr/>\n"
-            end
-
-            output += localIndent + "</li>\n"
-        }
+        output = indent + "<ul class=\"" + classNames + "\">\n" + output
         output += indent + "</ul>\n"
-        return output
+        return output, found
     end
 
     def render(context)
         rootChildren = super.strip!
         parsed = JSON.parse(rootChildren)
 
-        return renderTree(parsed, [])
+        output, found = renderTree(context, parsed, [])
+        return output
         # Write the output HTML string
         #   output =  "<div style=\"margin: 0 auto; padding: .8em 0;\"><script async "
         #   output += "src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\">"
