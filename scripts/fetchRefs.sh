4b7877497e79bca183654dc3a56e08ad64c67811
Simran Brucherseifer <simran@arangodb.com>
2019-10-24 14:21:51 +0200

    Add migration fast path, better ssh handling

--- scripts/fetchRefs.sh
+++ scripts/fetchRefs.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-set -e
-
-SOURCES=(
-    "https://@github.com/arangodb-helper/arangodb.git;arangodb-starter;docs-2019-10-16;docs/;Manual/"
-    "https://@github.com/arangodb/arangosync.git;arangosync;docs-update-2019-10-16;docs/;Manual/"
-    "https://@github.com/arangodb/kube-arangodb.git;kube-arangodb;docs-update-2019-10-16;docs/;Manual/"
-    "https://@github.com/arangodb/arangodb-java-driver.git;arangodb-java-driver;docs-update;docs/;Drivers/"
-    "https://@github.com/arangodb/arangodb-php.git;arangodb-php;docs-update-2019-10-16;docs/;Drivers/"
-    "https://@github.com/arangodb/arangodb-spark-connector.git;arangodb-spark-connector;master;docs/;Drivers/"
-    "https://@github.com/arangodb/arangojs.git;arangojs;docs-2019-10-16;docs/;Drivers/"
-    "https://@github.com/arangodb/go-driver.git;go-driver;master;docs/;Drivers/"
-    "https://@github.com/arangodb/spring-data.git;spring-data;docs-update-2019-10-16;docs/;Drivers/"
-)
+# Must not set -e or ssh stuff will abort the script
+#set -e
 
 GITAUTH="$1"
 
+if test "${GITAUTH}" == "migrate"; then
+    # Fast path, migration only
+    SOURCES=()
+else
+    SOURCES=(
+        "https://@github.com/arangodb-helper/arangodb.git;arangodb-starter;docs-2019-10-16;docs/;Manual/"
+        "https://@github.com/arangodb/arangosync.git;arangosync;docs-update-2019-10-16;docs/;Manual/"
+        "https://@github.com/arangodb/kube-arangodb.git;kube-arangodb;docs-update-2019-10-16;docs/;Manual/"
+        "https://@github.com/arangodb/arangodb-java-driver.git;arangodb-java-driver;docs-update;docs/;Drivers/"
+        "https://@github.com/arangodb/arangodb-php.git;arangodb-php;docs-update-2019-10-16;docs/;Drivers/"
+        "https://@github.com/arangodb/arangodb-spark-connector.git;arangodb-spark-connector;master;docs/;Drivers/"
+        "https://@github.com/arangodb/arangojs.git;arangojs;docs-2019-10-16;docs/;Drivers/"
+        "https://@github.com/arangodb/go-driver.git;go-driver;master;docs/;Drivers/"
+        "https://@github.com/arangodb/spring-data.git;spring-data;docs-update-2019-10-16;docs/;Drivers/"
+    )
+fi
+
+# Ensure agent is running
 if test "${GITAUTH}" == "git"; then
-    ssh-add -l &>/dev/null
+    ssh-add -l &> /dev/null
     if [ "$?" == 2 ]; then
         # Could not open a connection to your authentication agent.
-        eval `ssh-agent`
-        ssh-add -l &>/dev/null
-        if [ "$?" == 1 ]; then
-            # The agent has no identities.
-            ssh-add
+
+        # Load stored agent connection info
+        test -r ~/.ssh-agent && eval "$(<~/.ssh-agent)" > /dev/null
+        ssh-add -l &> /dev/null
+        if [ "$?" == 2 ]; then
+            # Start agent and store connection info
+            (umask 066; ssh-agent > ~/.ssh-agent)
+            eval "$(<~/.ssh-agent)" > /dev/null
         fi
+    fi
+
+    # Load identities
+    ssh-add -l &> /dev/null
+    if [ "$?" == 1 ]; then
+        # The agent has no identities.
+        ssh-add
     fi
 fi
 
-for source in ${SOURCES[@]}; do
+for source in "${SOURCES[@]}"; do
 
     REPO=$(echo     "$source" |cut -d ';' -f 1)
     CLONEDIR=$(echo "$source" |cut -d ';' -f 2)
