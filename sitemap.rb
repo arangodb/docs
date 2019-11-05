4ccc4fe49d9c167970ec911870893c976a275798
Simran <Simran-B@users.noreply.github.com>
2019-11-05 18:44:52 +0100

    Improve Sitemap generation + enable in Docker build.sh (#201)
    * Check canonical links, run sitemap.rb from build.sh
    
    * Force LF line endings for .sh scripts
    
    * Add Google site verification file
      Note: this must not be excluded in _config.yml to end up in _site/
    
    * Disable htmltest CheckDoctype to not fail for Google site verification file
    
    * Log beginning of sitemap generation
    
    * Run sitemap.rb in netlify.sh
      Allows us to inspect sitemap.xml in Netlify previews

--- sitemap.rb
+++ sitemap.rb
@@ -10,6 +10,8 @@ version = config['versions']['stable']
 baseurl = config['url'] + config['baseurl']
 dir = "_site/#{version}"
 
+puts("\nGenerating sitemap.xml\ndir = '#{dir}'\nbaseurl = '#{baseurl}'")
+
 if not Dir.exist?(dir)
     raise IOError, "Source directory does not exist: #{dir}"
 end
@@ -19,9 +21,26 @@ f = File.open('_site/sitemap.xml', 'w')
 f.write('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
 f.write('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + "\n")
 
-Dir.glob("#{dir}/**/*.html").sort_by{ |name| name }.each do |file|
-    next if File.size(file) < 2048 # Redirect pages are ~1KB, others >16KB
-    path = file.gsub(Regexp.new("^#{Regexp.quote(dir)}"), 'stable')
+Dir.glob("#{dir}/**/*.html").sort_by{ |name| name }.each do |filepath|
+    next if File.size(filepath) < 2048 # Redirect pages are ~1KB, others >16KB
+    path = filepath.gsub(Regexp.new("^#{Regexp.quote(dir)}"), 'stable')
+
+    # Above filesize check should already skip all redirect pages,
+    # but check the canonical URL anyway to avoid mismatches
+    canonical = ''
+    File.foreach(filepath) do |line|
+        m = line.match(/<link rel="canonical" href="([^"]*)">/)
+        if m and m.captures.length
+            canonical = m.captures[0]
+            break
+        end
+    end
+    url = "#{baseurl}/#{path.gsub(/\/index\.html$/, '/')}"
+    if url != canonical
+        puts("\nWARNING url and canonical mismatch in '#{filepath}', skipping:\n#{url}\n#{canonical}")
+        next
+    end
+
     f.write("<url><loc>#{baseurl}/#{path}</loc></url>\n")
 end
 
