3bf0c99ae27b0629ea77f2bce720157fec0de210
Simran Brucherseifer <simran@arangodb.com>
2019-11-06 15:12:01 +0100

    Add replace_last filter (unused)

--- _plugins/ExtraFilters.rb
+++ _plugins/ExtraFilters.rb
@@ -1,6 +1,20 @@
 module Jekyll
     module ExtraFilters
 
+        # Substitute last occurrence of string (no regexp support)
+        def replace_last(input, string, replacement = '')
+            input_s = input.to_s
+            string_s = string.to_s
+            return input_s if string_s.length == 0
+            start = input_s.rindex(string_s)
+            return input_s if start == nil
+            input_s[0, start] + replacement.to_s + (input_s[start + string_s.length..-1])
+            # Alternative implementation:
+            #pieces = input.to_s.rpartition string.to_s
+            #( pieces[(pieces.find_index string.to_s)] =  replacement.to_s ) rescue nil
+            #pieces.join
+        end
+
         # Figure out where we are at and compute navvar from folder path, e.g.
         # /3.4/ --> 34-manual
         # /3.4/aql/ --> 34-aql
