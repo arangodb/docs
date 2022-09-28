ae49e09cb23856279b0f6005d5a3f7ae4776cb54
dandimeo <daniele@arangodb.com>
2022-09-28 12:21:41 +0200

    #DOC-185 all render blocks done

--- site/themes/hugo-theme-relearn/static/js/version.js
+++ site/themes/hugo-theme-relearn/static/js/version.js
@@ -1,6 +1,25 @@
+getCurrentVersion();
 renderVersion();
 
-// Generic Markers Annotation
+function changeVersion() {
+  var version = localStorage.getItem('docs-version');
+  var versionSelector = document.getElementById("arangodb-version");
+  version  = versionSelector.options[versionSelector.selectedIndex].value;
+  localStorage.setItem('docs-version', version);
+  versionSelector.options[versionSelector.options.selectedIndex].selected = true;
+  location.reload();
+}
+
+function getCurrentVersion() {
+  var version = localStorage.getItem('docs-version');
+  var versionSelector = document.getElementById("arangodb-version");
+  for(let option of versionSelector.options) {
+    if (option.value == version) {
+      option.selected = true;
+    }
+  }
+}
+
 
 function renderVersion() {
     const versionBlocks = document.querySelectorAll('.version');
