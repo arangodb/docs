776b5bb4532c2343e46cd3cb0d03289354c04ce9
Simran Spiller <simran@arangodb.com>
2022-10-11 13:57:27 +0200

    Use _site/docs as output folder to make running htmltest easier

--- netlify.sh
+++ netlify.sh
@@ -5,16 +5,11 @@ set -e
 JEKYLL_ENV="netlify" jekyll clean
 JEKYLL_ENV="netlify" jekyll build --trace
 ruby sitemap.rb
-rm -rf htmltest
-mkdir -p htmltest
-# our baseUrl is /docs so we need to create that structure for htmltest
-# otherwise it would fail to find absolute links like /docs/3.5
-cp -a _site htmltest/docs
-cp _redirects htmltest
+cp _redirects _site
 
 (cd /tmp && \
     wget -nv https://github.com/wjdp/htmltest/releases/download/v0.16.0/htmltest_0.16.0_linux_amd64.tar.gz && \
     tar xvzf htmltest_0.16.0_linux_amd64.tar.gz
 )
 
-/tmp/htmltest -s
+/tmp/htmltest --skip-external
