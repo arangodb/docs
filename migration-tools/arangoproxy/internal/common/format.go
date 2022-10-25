7ebdd7de7a96e2cc2b823f7d51e78ff9597d7c63
dandimeo <daniele@arangodb.com>
2022-10-25 10:36:48 +0200

    #DOC-209 datasets in aql examples working

--- migration-tools/arangoproxy/internal/common/format.go
+++ migration-tools/arangoproxy/internal/common/format.go
@@ -10,11 +10,8 @@ import (
 */
 
 func FormatResponse(response *ExampleResponse) {
-	Logger.Printf("PRIMA DEL MATCH %s\n", response.Input)
-
 	codeComments := regexp.MustCompile(`(?m)~.*`) // Cut the ~... strings from the displayed input
 	response.Input = codeComments.ReplaceAllString(response.Input, "")
-	Logger.Printf("PRIMA DEL MATCH %s\n", response.Input)
 
 	re := regexp.MustCompile(`(?m)^\s*$\n`) // Cut all excessive spaces and newlines from output
 	response.Input = re.ReplaceAllString(response.Input, "")
