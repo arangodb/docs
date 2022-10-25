1c8c09a57e303587afa981e4575fb246bde3c196
dandimeo <daniele@arangodb.com>
2022-10-25 17:45:40 +0200

    #DOC-219 fix blank lines after frontmatter

--- migration-tools/migration/migration.py
+++ migration-tools/migration/migration.py
@@ -137,8 +137,7 @@ def processFile(filepath):
 	if "fileID" in infos[filepath]:
 		page.frontMatter.fileID = infos[filepath]["fileID"]
 
-
-	buffer = re.sub(r"-{3}\n[\w\s:/#,()?’'\@&\[\]\*.>-]+\n-{3}", '', buffer)
+	buffer = re.sub(r"---(.*?)---", '', buffer, 0, re.MULTILINE | re.DOTALL)
 
 	#Internal content
 	_processChapters(page, buffer)
@@ -150,7 +149,7 @@ def processFile(filepath):
 	return
 
 def _processFrontMatter(page, buffer):
-	frontMatterRegex = re.search(frontMatterCapture, buffer, re.MULTILINE | re.DOTALL)
+	frontMatterRegex = re.search(r"---(.*?)---", buffer, re.MULTILINE | re.DOTALL)
 	if not frontMatterRegex:
 		return		# TODO
 
@@ -191,6 +190,8 @@ def _processChapters(page, paragraph):
 	paragraph = migrate_hints(paragraph)
 	paragraph = migrateHTTPDocuBlocks(paragraph)
 	paragraph = migrateInlineDocuBlocks(paragraph)
+	paragraph = paragraph.lstrip("\n")
+
 	page.content = paragraph
 	return
 
@@ -228,7 +229,7 @@ class FrontMatter():
 		self.weight = 0
 
 	def toString(self):
-		return f"---\nfileID: {self.fileID}\ntitle: {self.title}\nmenuTitle: {self.menuTitle}\nweight: {self.weight}\ndescription: {self.description}\nlayout: default\n---\n"
+		return f"---\nfileID: {self.fileID}\ntitle: {self.title}\nweight: {self.weight}\ndescription: {self.description}\nlayout: default\n---\n"
 
 def get_weight(weight):
 	global currentWeight
